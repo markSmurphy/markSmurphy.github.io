0967aaf7c1773527ce0144f0c2bc17a0892cee2c
markSmurphy <mark.murphy@techemail.com>
2022-08-28 16:14:00 +0100

    Removed superfluous, default, content

--- docs/introduction.md
+++ docs/introduction.md
@@ -3,3 +3,5 @@ sidebar_position: 1
 ---
 
 # Introduction
+
+--- A brief introduction to what the Blog Posts are going to be about
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,9 +67,9 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'introduction',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Introduction',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
